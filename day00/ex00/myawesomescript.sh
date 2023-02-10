96ae5fe58c4aab4934a75cd6dc37811afd43d81d
Lamia <lboukrou@student.42.fr>
2023-02-10 15:21:54 +0100

    fix: check if arg exists

--- day00/ex00/myawesomescript.sh
+++ day00/ex00/myawesomescript.sh
@@ -3,4 +3,6 @@
 # "s" option stands for 'silent' so it won't print the download progress
 # "L" option can be added to follow all redirections, here we remove it to only keep one redrection
 # "I" to download only the http header
-curl -sI $1 | grep -i Location | cut -d ' ' -f 2
+if [$1]; then
+  curl -sI $1 | grep -i Location | cut -d ' ' -f 2
+fi
